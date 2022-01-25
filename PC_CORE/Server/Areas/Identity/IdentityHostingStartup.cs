04cf7323b5186899eb99f4721ad0d37fbf86e846
ARGANA JOHN ROMEO BRIAN LATEO <2002759E@student.tp.edu.sg>
2022-01-25 22:19:29 +0800

    created database

---
+++ PC_CORE/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PC_CORE.Server.Data;
+using PC_CORE.Server.Models;
+
+[assembly: HostingStartup(typeof(PC_CORE.Server.Areas.Identity.IdentityHostingStartup))]
+namespace PC_CORE.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- PC_CORE/Server/Models/ApplicationUser.cs
+++ PC_CORE/Server/Models/ApplicationUser.cs
@@ -8,5 +8,7 @@ namespace PC_CORE.Server.Models
 {
 	public class ApplicationUser : IdentityUser
 	{
-	}
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
 }
